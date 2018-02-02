05ef583e90494ee0dfe8b65182d526f4fe5bb326
Mo45 <kirillkrasin@gmail.com>
2018-02-02 11:57:46 +0200

    v1.0.3

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,8 @@
 <section class="section footer">
 <div class="container">
 <h6 class="float-left">&copy; <?php echo date("Y"); ?> &laquo;<?php echo SRV_NAME; ?>&raquo;</h6>
+<!-- Do NOT remove copyright -->
 <span class="float-right small">Designed & Coded by <a href="https://rust-servers.info">Kirill Krasin</a> &middot; v<?php echo VERSION; ?></span>
+<!-- Don't be a dick -->
 </div>
 </section>
