a30e799d8509aeb20a4b1533c4cd94f39f1ae3ff
Mo45 <kirillkrasin@gmail.com>
2018-02-02 11:54:18 +0200

    Full Page Video Styles

--- template/top.php
+++ template/top.php
@@ -1,4 +1,9 @@
 <section class="face" style="background-image: url( 'img/bg_0.jpg' );">
+<div class="fullvideo">
+	<video loop="" muted="" autoplay="" playsinline="">
+		<source src="bg.mp4" type="video/mp4">
+	</video>
+</div>
                 <div class="face-body">
                         <div class="container text-center">
                                 <h1 class="face-title one"><?php echo SRV_NAME; ?></h1>
