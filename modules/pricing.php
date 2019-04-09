44163ab525ad1f2c470f14a2e9e2602004152527
Kirill Krasin <kirillkrasin@gmail.com>
2019-04-09 08:05:20 +0000

    pricing table variations

--- modules/pricing.php
+++ modules/pricing.php
@@ -1,5 +1,16 @@
 <section class="section social text-center" id="pricing">
 <h2>Pricing Table</h2>
+<nav>
+  <div class="nav justify-content-center nav-tabs" id="nav-tab" role="tablist">
+    <a class="nav-item nav-link active" id="nav-monthly-tab" data-toggle="tab" href="#monthly" role="tab" aria-controls="nav-monhtly" aria-selected="true">Monthly</a>
+    <a class="nav-item nav-link" id="nav-lifetime-tab" data-toggle="tab" href="#lifetime" role="tab" aria-controls="nav-lifetime" aria-selected="false">Lifetime</a>
+    <a class="nav-item nav-link" id="nav-lite-tab" data-toggle="tab" href="#lite" role="tab" aria-controls="nav-lite" aria-selected="false">Lite</a>
+  </div>
+</nav>
+
+<div class="tab-content" id="pricingTypes">
+<div class="tab-pane fade show active" id="monthly" role="tabpanel" aria-labelledby="home-tab">
+
 <div class="panel pricing-table">
       <div class="pricing-plan">
         <h2 class="pricing-header">One</h2>
@@ -61,5 +72,139 @@
         <a href="#" class="btn btn-outline-light btn-lg mt-4" role="button">Go To Shop</a>
       </div>
       
-    </div>
+</div>
+</div><!-- tab pane -->
+<div class="tab-pane fade show" id="lifetime" role="tabpanel" aria-labelledby="home-tab">
+
+<div class="panel pricing-table">
+      <div class="pricing-plan">
+        <h2 class="pricing-header">One</h2>
+        <ul class="pricing-features">
+          <li class="pricing-features-item">Amazing Benefit #1</li>
+          <li class="pricing-features-item">Amazing Benefit #2</li>
+          <li class="pricing-features-item">Amazing Benefit #3</li>
+          <li class="pricing-features-item">—</li>
+          <li class="pricing-features-item">—</li>
+          <li class="pricing-features-item">—</li>
+        </ul>
+        <!-- You can change currency sign: fa-pound-sign, fa-ruble-sign, fa-dollar-sign, fa-euro-sign -->
+        <span class="pricing-price"><sup class="small"><!-- Currency sign: --><i class="fas fa-pound-sign"></i><!-- Currency sign /end --></sup> 35 <sub class="small">/for life</sub></span>
+        <a href="#" class="btn btn-outline-light btn-lg mt-4" role="button">Go To Shop</a>
+      </div>
+      
+      <div class="pricing-plan">
+        <h2 class="pricing-header">Two</h2>
+        <ul class="pricing-features">
+          <li class="pricing-features-item">Amazing Benefit #1</li>
+          <li class="pricing-features-item">Amazing Benefit #2</li>
+          <li class="pricing-features-item">Amazing Benefit #3</li>
+          <li class="pricing-features-item">Amazing Benefit #4</li>
+          <li class="pricing-features-item">—</li>
+          <li class="pricing-features-item">—</li>
+        </ul>
+        <!-- You can change currency sign: fa-pound-sign, fa-ruble-sign, fa-dollar-sign, fa-euro-sign -->
+        <span class="pricing-price"><sup class="small"><!-- Currency sign: --><i class="fas fa-pound-sign"></i><!-- Currency sign /end --></sup> 55 <sub class="small">/for life</sub></span>
+        <a href="#" class="btn btn-outline-light btn-lg mt-4" role="button">Go To Shop</a>
+      </div>
+      
+      <div class="pricing-plan">
+        <h2 class="pricing-header">Three</h2>
+        <ul class="pricing-features">
+          <li class="pricing-features-item">Amazing Benefit #1</li>
+          <li class="pricing-features-item">Amazing Benefit #2</li>
+          <li class="pricing-features-item">Amazing Benefit #3</li>
+          <li class="pricing-features-item">Amazing Benefit #4</li>
+          <li class="pricing-features-item">Amazing Benefit #5</li>
+          <li class="pricing-features-item">—</li>
+        </ul>
+        <!-- You can change currency sign: fa-pound-sign, fa-ruble-sign, fa-dollar-sign, fa-euro-sign -->
+        <span class="pricing-price"><sup class="small"><!-- Currency sign: --><i class="fas fa-pound-sign"></i><!-- Currency sign /end --></sup> 75 <sub class="small">/for life</sub></span>
+        <a href="#" class="btn btn-outline-light btn-lg mt-4" role="button">Go To Shop</a>
+      </div>
+
+      <div class="pricing-plan">
+        <h2 class="pricing-header">Four</h2>
+        <ul class="pricing-features">
+          <li class="pricing-features-item">Amazing Benefit #1</li>
+          <li class="pricing-features-item">Amazing Benefit #2</li>
+          <li class="pricing-features-item">Amazing Benefit #3</li>
+          <li class="pricing-features-item">Amazing Benefit #4</li>
+          <li class="pricing-features-item">Amazing Benefit #5</li>
+          <li class="pricing-features-item">Amazing Benefit #6</li>
+        </ul>
+        <!-- You can change currency sign: fa-pound-sign, fa-ruble-sign, fa-dollar-sign, fa-euro-sign -->
+        <span class="pricing-price"><sup class="small"><!-- Currency sign: --><i class="fas fa-pound-sign"></i><!-- Currency sign /end --></sup> 99 <sub class="small">/for life</sub></span>
+        <a href="#" class="btn btn-outline-light btn-lg mt-4" role="button">Go To Shop</a>
+      </div>
+      
+</div>
+</div><!-- tab pane -->
+
+<div class="tab-pane fade show" id="lite" role="tabpanel" aria-labelledby="home-tab">
+
+<div class="panel pricing-table">
+      <div class="pricing-plan">
+        <h2 class="pricing-header">One</h2>
+        <ul class="pricing-features">
+          <li class="pricing-features-item">Amazing Benefit #1</li>
+          <li class="pricing-features-item">Amazing Benefit #2</li>
+          <li class="pricing-features-item">Amazing Benefit #3</li>
+          <li class="pricing-features-item">—</li>
+          <li class="pricing-features-item">—</li>
+          <li class="pricing-features-item">—</li>
+        </ul>
+        <!-- You can change currency sign: fa-pound-sign, fa-ruble-sign, fa-dollar-sign, fa-euro-sign -->
+        <span class="pricing-price"><sup class="small"><!-- Currency sign: --><i class="fas fa-pound-sign"></i><!-- Currency sign /end --></sup> 3 <sub class="small">/month</sub></span>
+        <a href="#" class="btn btn-outline-light btn-lg mt-4" role="button">Go To Shop</a>
+      </div>
+      
+      <div class="pricing-plan">
+        <h2 class="pricing-header">Two</h2>
+        <ul class="pricing-features">
+          <li class="pricing-features-item">Amazing Benefit #1</li>
+          <li class="pricing-features-item">Amazing Benefit #2</li>
+          <li class="pricing-features-item">Amazing Benefit #3</li>
+          <li class="pricing-features-item">Amazing Benefit #4</li>
+          <li class="pricing-features-item">—</li>
+          <li class="pricing-features-item">—</li>
+        </ul>
+        <!-- You can change currency sign: fa-pound-sign, fa-ruble-sign, fa-dollar-sign, fa-euro-sign -->
+        <span class="pricing-price"><sup class="small"><!-- Currency sign: --><i class="fas fa-pound-sign"></i><!-- Currency sign /end --></sup> 7 <sub class="small">/month</sub></span>
+        <a href="#" class="btn btn-outline-light btn-lg mt-4" role="button">Go To Shop</a>
+      </div>
+      
+      <div class="pricing-plan">
+        <h2 class="pricing-header">Three</h2>
+        <ul class="pricing-features">
+          <li class="pricing-features-item">Amazing Benefit #1</li>
+          <li class="pricing-features-item">Amazing Benefit #2</li>
+          <li class="pricing-features-item">Amazing Benefit #3</li>
+          <li class="pricing-features-item">Amazing Benefit #4</li>
+          <li class="pricing-features-item">Amazing Benefit #5</li>
+          <li class="pricing-features-item">—</li>
+        </ul>
+        <!-- You can change currency sign: fa-pound-sign, fa-ruble-sign, fa-dollar-sign, fa-euro-sign -->
+        <span class="pricing-price"><sup class="small"><!-- Currency sign: --><i class="fas fa-pound-sign"></i><!-- Currency sign /end --></sup> 9 <sub class="small">/month</sub></span>
+        <a href="#" class="btn btn-outline-light btn-lg mt-4" role="button">Go To Shop</a>
+      </div>
+
+      <div class="pricing-plan">
+        <h2 class="pricing-header">Four</h2>
+        <ul class="pricing-features">
+          <li class="pricing-features-item">Amazing Benefit #1</li>
+          <li class="pricing-features-item">Amazing Benefit #2</li>
+          <li class="pricing-features-item">Amazing Benefit #3</li>
+          <li class="pricing-features-item">Amazing Benefit #4</li>
+          <li class="pricing-features-item">Amazing Benefit #5</li>
+          <li class="pricing-features-item">Amazing Benefit #6</li>
+        </ul>
+        <!-- You can change currency sign: fa-pound-sign, fa-ruble-sign, fa-dollar-sign, fa-euro-sign -->
+        <span class="pricing-price"><sup class="small"><!-- Currency sign: --><i class="fas fa-pound-sign"></i><!-- Currency sign /end --></sup> 13 <sub class="small">/month</sub></span>
+        <a href="#" class="btn btn-outline-light btn-lg mt-4" role="button">Go To Shop</a>
+      </div>
+      
+</div>
+</div><!-- tab pane -->
+
+</div><!-- tab content -->
 </section>
