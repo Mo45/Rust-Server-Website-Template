bd6e6bb0fa26cb340728105c097b4cc12e39ac26
Kirill Krasin <kirillkrasin@gmail.com>
2019-04-12 06:54:32 +0000

    added sponsors in footer

--- template/footer.php
+++ template/footer.php
@@ -2,7 +2,8 @@
 <div class="container">
 <h6 class="float-left">&copy; <?php echo date("Y"); ?> &laquo;<?php echo SRV_NAME; ?>&raquo;</h6>
 <!-- Do NOT remove copyright -->
-<span class="float-right small">Designed & Coded by <a href="https://github.com/Mo45">Kirill Krasin</a> &middot; <i class="fab fa-github"></i> v<a href="https://github.com/Mo45/Rust-Server-Website-Template"><?php echo VERSION; ?></a></span>
+<!-- Become my Patron ( https://www.patreon.com/rustserversinfo ) to remove this links or if you want to be here -->
+<span class="float-right small" id="footer">Created by <a href="https://github.com/Mo45">Kirill Krasin</a> &middot; Sponsored by: <a href="http://therustcity.com">The Rust City</a> &middot; <a href="https://theNords.no">theNords.no</a></span>
 <!-- Don't be a dick -->
 </div>
 </section>
