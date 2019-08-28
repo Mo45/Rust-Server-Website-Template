2afdcaef483fdc07886eaf6c11463e8038430981
Kirill Krasin <kirillkrasin@gmail.com>
2019-08-28 14:40:43 +0300

    Added connection modal & various fixes

--- includes.php
+++ includes.php
@@ -7,11 +7,20 @@
 	$json_string = file_get_contents($json_url);
 	$parsed_json = json_decode($json_string,true);
 		$s1_name	= $parsed_json['name'];
+		if($s1_name == ""){$s1_name = "Server Offline";};//Offline server name fix
 		$s1_status	= $parsed_json['is_online'];
-			if($s1_status == "1"){$s1_status = "Online";$s1_button = "class='btn btn-outline-success btn-lg'><i class='fa fa-play-circle' aria-hidden='true'></i> Connect</a> ";}
-            else{$s1_status = "Offline";$s1_button = "class='btn btn-outline-danger btn-lg disabled'><i class='fa fa-exclamation-circle' aria-hidden='true'></i> Offline</a>&nbsp;";};
+			if($s1_status == "1"){$s1_status = "Online";
+			$s1_button = "class='btn btn-outline-success btn-lg'><i class='fa fa-play-circle' aria-hidden='true'></i> Connect</a> ";
+			$s1_connect_button = "<button data-toggle='modal' data-target='#ServerConnect1' type='button' class='btn btn-outline-success btn-lg'><i class='fab fa-steam-symbol'></i> Connect</button>";
+		}
+            else{$s1_status = "Offline";
+            $s1_button = "class='btn btn-outline-danger btn-lg disabled'><i class='fa fa-exclamation-circle' aria-hidden='true'></i> Offline</a>&nbsp;";
+            $s1_connect_button = "<button type='button' class='btn btn-outline-danger btn-lg disabled'><i class='fa fa-exclamation-circle'></i> Offline</button>";
+        };
 		$s1_cur		= $parsed_json['players'];
+		if($s1_cur == ""){$s1_cur = "0";};//Offline server players fix
 		$s1_max		= $parsed_json['maxplayers'];
+		if($s1_max == ""){$s1_max = "0";};//Offline server players fix
 		$s1_url		= $parsed_json['url'];
 		if(MODULE_SERVERS_V2 == "ON"){$s1_img = "img/serverlogo.png";};//Remove PHP Notice in logs
 		if(MODULE_SERVERS_V2 == "OFF"){
@@ -26,11 +35,20 @@
         $json_string = file_get_contents($json_url);
         $parsed_json = json_decode($json_string,true);
 		$s2_name        = $parsed_json['name'];
+		if($s2_name == ""){$s2_name = "Server Offline";};//Offline server name fix
         $s2_status      = $parsed_json['is_online'];
-            if($s2_status == "1"){$s2_status = "Online";$s2_button = "class='btn btn-outline-success btn-lg'><i class='fa fa-play-circle' aria-hidden='true'></i> Connect</a>&nbsp;";}
-			else{$s2_status = "Offline";$s2_button = "class='btn btn-outline-danger btn-lg disabled'><i class='fa fa-exclamation-circle' aria-hidden='true'></i> Offline</a> ";};
+            if($s2_status == "1"){$s2_status = "Online";
+            $s2_button = "class='btn btn-outline-success btn-lg'><i class='fa fa-play-circle' aria-hidden='true'></i> Connect</a>&nbsp;";
+            $s2_connect_button = "<button data-toggle='modal' data-target='#ServerConnect2' type='button' class='btn btn-outline-success btn-lg'><i class='fab fa-steam-symbol'></i> Connect</button>";
+        }
+			else{$s2_status = "Offline";
+			$s2_button = "class='btn btn-outline-danger btn-lg disabled'><i class='fa fa-exclamation-circle' aria-hidden='true'></i> Offline</a> ";
+			$s2_connect_button = "<button type='button' class='btn btn-outline-danger btn-lg disabled'><i class='fa fa-exclamation-circle'></i> Offline</button>";
+		};
         $s2_cur         = $parsed_json['players'];
+        if($s2_cur == ""){$s2_cur = "0";};//Offline server players fix
         $s2_max         = $parsed_json['maxplayers'];
+        if($s2_max == ""){$s2_max = "0";};//Offline server players fix
         $s2_url		= $parsed_json['url'];
         if(MODULE_SERVERS_V2 == "ON"){$s2_img = "img/serverlogo.png";};//Remove PHP Notice in logs
         if(MODULE_SERVERS_V2 == "OFF"){
