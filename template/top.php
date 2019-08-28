8b86fc2edfba34b7c1360d45780d04e68e3a7546
Kirill Krasin <kirillkrasin@gmail.com>
2019-08-28 11:45:52 +0300

    added tip how to use image instead of H1 and H2

--- template/top.php
+++ template/top.php
@@ -3,14 +3,15 @@
 <!-- Specify the path to your video file, because this is video from Facepunch website :) -->
 <div class="fullvideo">
         <video preload="auto" autoplay="autoplay" muted="muted" loop="loop" playsinline="">
-        	<source type="video/mp4" src="bg.mp4">
+        	<source type="video/mp4" src="<?php echo SITE_URL; ?>/bg.mp4">
         </video>
 </div>
 <?php ;}else{ ?>
-<section class="face" style="background-image: url( '//abjwokxjen.cloudimg.io/cdn/n/n/demo.ruster.site/img/bg_0.jpg' );">
+<section class="face" style="background-image: url( '<?php echo SITE_URL; ?>/img/bg_0.jpg' );">
 <?php ;};?>
                 <div class="face-body">
                         <div class="container text-center">
+                        	<!-- If you want to use image instead of text, take a look here >>> https://gist.github.com/Mo45/fee60eabbd598c07b29a5967c7998146 -->
                                 <h1 class="face-title one"><?php echo SRV_NAME; ?></h1>
                                 <h2 class="face-subtitle"><?php echo SRV_SLOGAN; ?></h2>
                         </div>
