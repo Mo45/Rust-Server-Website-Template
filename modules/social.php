9e9254cd5df9f192ddc6ed890476784e4556428f
Kirill Krasin <kirillkrasin@gmail.com>
2020-12-07 10:03:57 +0300

    removed unused variables

--- modules/social.php
+++ modules/social.php
@@ -1,9 +1,6 @@
 <section class="section social text-center" id="social">
-<!-- paid modules -->
-<!-- ?php include_once('paid/twitter.php');include_once('paid/vk.php');? -->
-<!-- paid modules, /end -->
 <h1>Follow us</h1>
 <a href="<?php echo SRV_FACEBOOK; ?>" target="_blank" class="btn btn-outline-light"><i class="fab fa-facebook-f fa-fw"></i></a>
-<a href="<?php echo SRV_VK; ?>" target="_blank" class="btn btn-outline-light"><i class="fab fa-vk fa-fw"></i> <?php echo $vk_total;?></a>
-<a href="https://twitter.com/<?php echo SRV_TWITTER; ?>" target="_blank" class="btn btn-outline-light"><i class="fab fa-twitter fa-fw"></i> <?php echo $twitter_followers;?></a>
+<a href="<?php echo SRV_VK; ?>" target="_blank" class="btn btn-outline-light"><i class="fab fa-vk fa-fw"></i></a>
+<a href="https://twitter.com/<?php echo SRV_TWITTER; ?>" target="_blank" class="btn btn-outline-light"><i class="fab fa-twitter fa-fw"></i></a>
 </section>
