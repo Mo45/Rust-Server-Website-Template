31232e3e10589f6108370874205d282a4e68467b
Kirill Krasin <kirillkrasin@gmail.com>
2024-05-26 16:04:40 +0300

    css var for main colors + fixes

--- template/footer.php
+++ template/footer.php
@@ -1,9 +1,27 @@
+<section class="description">
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-1 text-center description-icon">
+<img src="https://static.mosharust.com/images/rust-marque-footer.svg" id="desc-icon" alt="<?php echo SRV_NAME; ?>" width="64" height="64">
+</div>
+
+<div class="col-md-11 description-text">
+<h2><?php echo SRV_NAME; ?> - <?php echo SRV_SLOGAN; ?></h2>
+<h3>Some cool info about your project. Hardware specs, or community stats. Etc. You can edit it in <strong>template/footer.php</strong> file</h3>
+</div>
+
+</div>
+</div>
+</section>
+
 <section class="section footer">
 <div class="container">
 <h6 class="float-start">&copy; <?php echo date("Y"); ?> &laquo;<?php echo SRV_NAME; ?>&raquo;</h6>
 <!-- Do NOT remove copyright -->
 <!-- Send me at least a tip ( https://boosty.to/krasinkirill/donate ) to remove this links or if you want to be here -->
-<span class="float-end small" id="footer">Created by <a href="https://github.com/Mo45">Kirill Krasin</a></span>
+<span class="float-end small" id="footer">Created by <a target="_blank" data-bs-toggle="tooltip" data-bs-placement="left" title="Kirill Krasin" rel="author" href="https://github.com/Mo45"><i class="fa-brands fa-github"></i> Kirill Krasin</a></span>
 <!-- Don't be a dick -->
 </div>
 </section>
